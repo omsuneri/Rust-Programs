d0cf02f9a37405793ae8c174f727d2671721c54b
omsuneri <omsuneri@gmail.com>
2024-08-24 15:42:28 +0530

    1st program

---
+++ Cargo-package-manager-sample/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+/Users/omsuneri/Desktop/Rust\ Programs\ /Rust-Programs/helloworld/target/debug/helloworld: /Users/omsuneri/Desktop/Rust\ Programs\ /Rust-Programs/helloworld/src/main.rs
---
+++ hello.rs
@@ -0,0 +1,9 @@
+
+fn main() {
+    println!("Hello World");
+    om();
+}
+
+fn om() {
+    println!("Heloo");
+}
